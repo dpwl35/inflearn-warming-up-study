df3e31a214d3ff534d354de58e968779491029b2
dpwl35 <dpwl9435@gmail.com>
2024-09-29 20:13:16 +0900

    Feat : tab 기능 추가

--- 01-food-menu/main.js
+++ 01-food-menu/main.js
@@ -1,5 +1,4 @@
-import { data } from './data.js'; // data.js에서 데이터 가져오기
-
+import { data } from './data.js';
 // 메뉴
 function initializeMenu() {
   const categories = ['poke', 'bowl', 'wrap', 'side'];
@@ -8,7 +7,6 @@ function initializeMenu() {
     const menuContainer = document.getElementById(category);
 
     data[category].forEach((item) => {
-      // 메뉴 항목 생성
       const menuItem = document.createElement('li');
       menuItem.classList.add('menu');
 
@@ -23,7 +21,6 @@ function initializeMenu() {
         <p class="menu-description">${item.description}</p>
       `;
 
-      // 컨테이너에 추가
       menuContainer.appendChild(menuItem);
     });
   });
@@ -35,17 +32,13 @@ function handleTabClick() {
 
   tabItems.forEach((tab, index) => {
     tab.addEventListener('click', () => {
-      // 모든 탭에서 active 클래스 제거
       tabItems.forEach((item) => item.classList.remove('active'));
 
-      // 클릭된 탭에만 active 클래스 추가
       tab.classList.add('active');
 
-      // 모든 카테고리 내용 숨기기
       const tabContents = document.querySelectorAll('.tab-content');
       tabContents.forEach((content) => content.classList.remove('active'));
 
-      // 선택된 카테고리 내용 표시
       const categories = ['poke', 'bowl', 'wrap', 'side'];
       const selectedCategory = categories[index]; // 클릭된 카테고리
 
@@ -55,5 +48,5 @@ function handleTabClick() {
 }
 
 // 초기화 함수 실행
-initializeMenu(); // HTML 초기 생성
-handleTabClick(); // 탭 클릭 이벤트 처리
+initializeMenu();
+handleTabClick();
