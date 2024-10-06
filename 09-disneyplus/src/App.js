d63448f844398ad73b2e82d0277a739b287f4582
dpwl35 <dpwl9435@gmail.com>
2024-10-06 16:26:13 +0900

    init: Initialize project

---
+++ 09-disneyplus/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className='flex items-center justify-center w-screen h-screen bg-blue-100'>
+      <div className='w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg'>
+        <div className='flex justify-between mb-3 '>
+          <h1>디즈니 플러스</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
