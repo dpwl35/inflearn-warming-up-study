6679cd7dbc236859ce8620da8d1beab3c8726d0e
dpwl35 <dpwl9435@gmail.com>
2024-10-09 15:22:34 +0900

    feat: 수정

---
+++ 09-disneyplus/src/pages/MainPage/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Banner from '../../components/Banner';
+import Row from '../../components/Row';
+import requests from '../../api/requests';
+
+export default function MainPage() {
+  return (
+    <div>
+      <Banner />
+
+      <Row title='Trending Now' id='TN' fetchUrl={requests.fetchTrending} />
+      <Row
+        title='NETFLIX ORIGINALS'
+        id='NO'
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+    </div>
+  );
+}
