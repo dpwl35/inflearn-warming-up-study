a0bd1e80da57a66ef15e2aa3847ac656a6262ba3
dpwl35 <dpwl9435@gmail.com>
2024-10-09 16:34:55 +0900

    style: add Genre UI

--- 09-disneyplus/src/pages/MainPage/index.js
+++ 09-disneyplus/src/pages/MainPage/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Banner from '../../components/Banner';
 import Row from '../../components/Row';
 import requests from '../../api/requests';
+import Genre from '../../components/Genre';
 
 export default function MainPage() {
   return (
     <div>
       <Banner />
-
+      <Genre />
       <Row title='Trending Now' id='TN' fetchUrl={requests.fetchTrending} />
       <Row
         title='Top Rated'
