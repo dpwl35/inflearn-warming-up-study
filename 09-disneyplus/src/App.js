e4c3917df43edcc4c829899a26f32737693d4150
dpwl35 <dpwl9435@gmail.com>
2024-10-09 15:19:46 +0900

    feat: Routes 적용

--- 09-disneyplus/src/App.js
+++ 09-disneyplus/src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { Outlet, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Nav from './components/Nav';
+import Footer from './components/Footer';
+import MainPage from './pages/MainPage';
+import DetailPage from './pages/DetailPage';
+import SearchPage from './pages/SearchPage';
+
+const Layout = () => {
+  return (
+    <div>
+      <Nav />
+
+      <Outlet />
+
+      <Footer />
+    </div>
+  );
+};
 
 export default function App() {
   return (
     <div className='wrap'>
-      <Nav />
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path=':movieId' element={<DetailPage />} />
+          <Route path='search' element={<SearchPage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
