0c0d48be326f000af2677da0cd83037a4ae51d93
dpwl35 <dpwl9435@gmail.com>
2024-10-09 15:21:42 +0900

    feat: 영화 클릭 modal open

--- 09-disneyplus/src/components/Nav.js
+++ 09-disneyplus/src/components/Nav.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Nav.css';
+import { useNavigate } from 'react-router-dom';
 
 export default function Nav() {
   const menuItems = ['홈', '검색', '관심 콘텐츠', '영화', '시리즈', '오리지널'];
+  const [searchValue, setsearchValue] = useState('');
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setsearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
 
   return (
     <header>
@@ -12,6 +20,14 @@ export default function Nav() {
             <img
               src='https://static-assets.bamgrid.com/product/disneyplus/images/disney-plus-logo-white-update.f384bde4d5a7f1f455e2dc7d8d4348ae.png'
               alt='Disney Plus Logo'
+            />
+          </li>
+          <li>
+            <input
+              value={searchValue}
+              onChange={handleChange}
+              type='text'
+              placeholder='영화를 검색해주세요.'
             />
           </li>
           {menuItems.map((item, index) => (
