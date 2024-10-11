3f48d08e74b379f78dadb5d7d98cf6ba82ca6fed
dpwl35 <dpwl9435@gmail.com>
2024-10-12 03:13:06 +0900

    feat: 메인 링크 추가

--- 10-pokeapi/src/components/Nav.js
+++ 10-pokeapi/src/components/Nav.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Nav() {
   return (
     <header className='px-9 w-full fixed top-0 left-0 text-lg bg-[#ef5350]'>
-      <h1 className='pb-4 pt-5  text-white font-bold text-2xl'>Pokemon</h1>
+      <h1 className='pb-4 pt-5 text-white font-bold text-2xl'>
+        <Link to='/'>Pokemon</Link>
+      </h1>
     </header>
   );
 }
