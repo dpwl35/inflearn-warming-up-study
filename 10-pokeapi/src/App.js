106ab2ef043fd20760c1c1d939f979d1574d5093
dpwl35 <dpwl9435@gmail.com>
2024-10-12 03:12:08 +0900

    feat: Route 추가

--- 10-pokeapi/src/App.js
+++ 10-pokeapi/src/App.js
@@ -1,14 +1,23 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Nav from './components/Nav';
-import PokemonList from './components/PokemonList';
-import SearchInput from './components/SearchInput';
+import Main from './pages/Main';
+import Detail from './pages/Detail';
 
 function App() {
   return (
     <div className='App'>
       <Nav />
-      <SearchInput />
-      <PokemonList />
+      <div className='wrap mt-[72px]'>
+        <Routes>
+          {/* 메인 페이지 경로 */}
+          <Route path='/' element={<Main />} />
+
+          {/* 포켓몬 상세 페이지 경로 */}
+          <Route path='/:id' element={<Detail />} />
+        </Routes>
+      </div>
     </div>
   );
 }
