4696e5fee091c5016b890a8929bbec3752febb86
dpwl35 <dpwl9435@gmail.com>
2024-10-12 03:12:45 +0900

    feat: img 주소 수정

--- 10-pokeapi/src/api/requests.js
+++ 10-pokeapi/src/api/requests.js
@@ -1,8 +1,9 @@
 const requests = {
   fetchPokemonById: (id) => `/pokemon/${id}`,
   fetchPokemonByName: (name) => `/pokemon/${name}`,
-  fetchPokemonImage: (id) =>
-    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
+  fetchPokemonImage:
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon',
+  fetchPokemonSpecies: (name) => `/pokemon-species/${name}`,
 };
 
 export default requests;
