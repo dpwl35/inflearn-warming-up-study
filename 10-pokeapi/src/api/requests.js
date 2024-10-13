28452aec554769d25571f4ce78499f585e3923d1
dpwl35 <dpwl9435@gmail.com>
2024-10-13 23:02:08 +0900

    feat : img 관련 경로 추가

--- 10-pokeapi/src/api/requests.js
+++ 10-pokeapi/src/api/requests.js
@@ -3,7 +3,10 @@ const requests = {
   fetchPokemonByName: (name) => `/pokemon/${name}`,
   fetchPokemonImage:
     'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon',
+  fetchPokemonImageGif:
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated',
   fetchPokemonSpecies: (name) => `/pokemon-species/${name}`,
+  fetchPokemonType: (typeName) => `/type/${typeName}`,
 };
 
 export default requests;
