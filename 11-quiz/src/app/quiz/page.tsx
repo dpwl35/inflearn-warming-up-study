cc7295e367b7bf5b241cbd776884b50b5a9ed45d
dpwl35 <dpwl9435@gmail.com>
2024-10-14 01:31:28 +0900

    style: 테스트 UI 추가

--- 11-quiz/src/app/quiz/page.tsx
+++ 11-quiz/src/app/quiz/page.tsx
@@ -1,3 +1,25 @@
+'use client';
+import React, { useState } from 'react';
+
 export default function QuizPage() {
-  return <div>퀴즈맞추기</div>;
+  const [selectedSubject, setSelectedSubject] = useState<string>('');
+
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedSubject(event.target.value);
+  };
+
+  return (
+    <div className='test'>
+      <p>시험</p>
+      <div className='test-area'>
+        <p>상태선택</p>
+        <select onChange={handleSelectChange}>
+          <option value=''>시험문제를 선택하세요.</option>
+          <option value='math'>수학</option>
+          <option value='korean'>국어</option>
+        </select>
+        <button type='button'>연습 테스트 시작</button>
+      </div>
+    </div>
+  );
 }
