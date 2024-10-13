b4b2cd05324a645eedef82a7daeb25a5aa17262c
dpwl35 <dpwl9435@gmail.com>
2024-10-14 01:25:23 +0900

    feat: select 선택 값 따라 문제 보여주기

--- 11-quiz/src/app/state/page.tsx
+++ 11-quiz/src/app/state/page.tsx
@@ -1,3 +1,33 @@
+'use client';
+import React, { useState } from 'react';
+
 export default function StatePage() {
-  return <div>StatePage</div>;
+  const [selectedSubject, setSelectedSubject] = useState<string>(''); // 상태 관리
+
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedSubject(event.target.value);
+  };
+
+  return (
+    <div>
+      <select onChange={handleSelectChange}>
+        <option value=''>과목을 선택하세요</option>
+        <option value='math'>수학</option>
+        <option value='korean'>국어</option>
+      </select>
+
+      {/* 선택된 값에 따른 문제 표시 */}
+      {selectedSubject === '' && <div>문제를 선택하세요</div>}
+      {selectedSubject === 'math' && <Math />}
+      {selectedSubject === 'korean' && <Korean />}
+    </div>
+  );
+}
+
+function Math() {
+  return <div>수학 문제</div>;
+}
+
+function Korean() {
+  return <div>국어 문제</div>;
 }
